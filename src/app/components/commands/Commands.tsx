f0d8dd3c2196dc1f76309cd6c7bf80b9a82e9fd9
pabol139 <pabloski139@gmail.com>
2025-06-22 17:42:46 +0200

    Clean unused functions

--- src/app/components/commands/Commands.tsx
+++ src/app/components/commands/Commands.tsx
@@ -11,34 +11,6 @@ export default function CommandsSection() {
   const { pathObject, updateCommands } = usePathObject();
   const commands = pathObject.commands;
 
-  const handlerEnter = (id_command: string) => {
-    const newCommands = commands.map((command) => {
-      if (command.id !== id_command) return { ...command, hovered: false }; // Return unmodified command
-
-      return { ...command, hovered: true }; // Return new object
-    });
-
-    updateCommands(newCommands);
-  };
-
-  const handlerLeave = () => {
-    const newCommands = commands.map((command) => {
-      return { ...command, hovered: false }; // Return new object
-    });
-
-    updateCommands(newCommands);
-  };
-
-  const handleDown = (id_command: string) => {
-    const newCommands = commands.map((command) => {
-      if (command.id !== id_command) return { ...command, selected: false }; // Return unmodified command
-
-      return { ...command, selected: true }; // Return new object
-    });
-
-    updateCommands(newCommands);
-  };
-
   return (
     <CollapsedSection title="Commands">
       <ul className="pb-5  gap-2 flex flex-col">
